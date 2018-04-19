27df0520fc4108565c8dddbeef0ccb37a6cd4dd8
leoberenger <leoberenger@gmail.com>
2018-04-19 16:20:28 +0200

    Added MapsFragment

--- app/src/main/java/com/openclassrooms/go4lunch/controllers/activities/MainActivity.java
+++ app/src/main/java/com/openclassrooms/go4lunch/controllers/activities/MainActivity.java
@@ -1,23 +1,65 @@
 package com.openclassrooms.go4lunch.controllers.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.openclassrooms.go4lunch.R;
+import com.openclassrooms.go4lunch.api.GMPlacesStreams;
+import com.openclassrooms.go4lunch.api.models.PlacesAPI;
 import com.openclassrooms.go4lunch.controllers.fragments.RestoListFragment;
+import com.openclassrooms.go4lunch.utils.PermissionUtils;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback{
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private boolean mPermissionDenied = false;
+
+    private GoogleMap mMap;
+    private Disposable mDisposable;
+    private List<PlacesAPI.Result> places;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
         ButterKnife.bind(this);
 
-        configureAndShowSearchFragment();
-        //configureAndShowMapFragment();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        this.executeHttpRequestWithRetrofit();
+
+        //configureAndShowSearchFragment();
     }
 
     private void configureAndShowSearchFragment() {
@@ -29,12 +71,145 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
     }
 
-    private void configureAndShowMapFragment() {
-
-        MapsFragment mapsFragment = new MapsFragment();
-
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.activity_main_frame_layout, mapsFragment)
-                .commit();
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        this.disposeWhenDestroy();
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        enableMyLocation();
+        positionLocationButton();
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    private void positionLocationButton(){
+        View locationButton = ((View) this.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        rlp.setMargins(0, 0, 30, 30);
+    }
+
+    private void executeHttpRequestWithRetrofit(){
+        this.mDisposable = GMPlacesStreams.streamFetchNearbyRestaurants("48.396396,-4.477549")
+                .subscribeWith(new DisposableObserver<PlacesAPI>(){
+                    @Override
+                    public void onNext(PlacesAPI places) {
+                        Log.e("MapsActivity", "On Next");
+                        updateUI(places);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("MapsActivity", "On Error"+Log.getStackTraceString(e));
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e("MapsActivity", "On Complete");
+                    }
+                });
+    }
+
+    private void disposeWhenDestroy(){
+        if(this.mDisposable != null && !this.mDisposable.isDisposed())
+            this.mDisposable.dispose();
+    }
+
+    // -----------------
+    // UPDATE UI
+    // -----------------
+
+    void updateUI(PlacesAPI results){
+        mMap.clear();
+        // This loop will go through all the results and add marker on each location.
+        for (int i = 0; i < results.getResults().size(); i++) {
+
+            Double lat = results.getResults().get(i).getGeometry().getLocation().getLat();
+            Double lng = results.getResults().get(i).getGeometry().getLocation().getLng();
+            String placeName = results.getResults().get(i).getName();
+            String vicinity = results.getResults().get(i).getVicinity();
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            LatLng latLng = new LatLng(lat, lng);
+
+            // Position of Marker on Map
+            markerOptions.position(latLng);
+            // Adding Title to the Marker
+            markerOptions.title(placeName + " : " + vicinity);
+            // Adding Marker to the Camera.
+            Marker m = mMap.addMarker(markerOptions);
+            // Adding colour to the marker
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            // move map camera
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+        }
+    }
+
 }
