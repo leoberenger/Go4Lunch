a9664b9f76482271ba754876e143da1b6f3468ca
leoberenger <leoberenger@gmail.com>
2018-04-27 11:09:34 +0200

    Added WorkmatesMgr setting and getting List of Users

---
+++ app/src/main/java/com/openclassrooms/go4lunch/managers/WorkmatesMgr.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.go4lunch.managers;
+
+import com.openclassrooms.go4lunch.models.User;
+import com.openclassrooms.go4lunch.models.googlemaps.PlacesAPI;
+
+import java.util.List;
+
+public class WorkmatesMgr {
+
+    private static final WorkmatesMgr ourInstance = new WorkmatesMgr();
+
+    private List<User> workmates;
+
+    public static WorkmatesMgr getInstance() {
+        return ourInstance;
+    }
+
+    private WorkmatesMgr() {
+    }
+
+    public List<User> getWorkmates() {
+        return workmates;
+    }
+
+    public void setWorkmates(List<User> workmates) {
+        this.workmates = workmates;
+    }
+
+}
